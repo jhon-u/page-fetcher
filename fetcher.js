d86bd091848e20a81bf9166dcbf21a965398fe4f
Jhon Useche <jhon.useche@telus.com>
2022-11-23 18:47:20 -0700

    added error handling functions for invalid path and invalid url

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,10 @@
-// There are two operations in this problem which will take an unknown amount of time:
-
-//  * You need to make an http request and wait for the response.
-//  * After the http request is complete, you need to take the data you receive and write it to a file in your local filesystem.
+/*
+* Page Downloader is a node app that takes two command line arguments:
+* @param {url} a valid URL.
+* @param {path} a local file path.
+*
+* Use from the command line: $ node fetcher.js http://www.example.edu/ ./index.html
+*/
 
 const request = require('request');
 const fs = require('fs');
@@ -9,26 +12,30 @@ const readline = require('readline');
 
 const fetcher = (url, path) => {
   request(url, function(error, response, body) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  
-    fs.open(path, 'wx', function(err, fd) {
-      if (err) {
-        if (err.code === 'EEXIST') {
-          return fileAlreadyExists(path, body);
+    if (!error && response.statusCode === 200) {
+      fs.open(path, 'wx', function(err) {
+        if (err) {
+          if (err.code === 'EEXIST') {
+            return fileAlreadyExists(path, body);
+          }
+          if (err.code === 'ENOENT') {
+            return console.log(`File Path is Invalid: no such file or directory, open ${path}`);
+          }
+          return console.log('ERROR: ' + err);
         }
-        console.log('ERROR: ' + err);
-      } else {
+        
         return writeFileToSystem(path, body);
-      }
-    });
+      });
+    } else {
+      return console.log(`${error}`);
+    }
   });
 };
 
 const writeFileToSystem = (path, body) => {
   fs.writeFile(path, body, err => {
     if (err) {
-      console.error(err);
+      return console.error(err);
     }
     console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
   });
@@ -39,7 +46,7 @@ const fileAlreadyExists = (path, body) => {
     input: process.stdin,
     output: process.stdout
   });
-  rl.question(`The file at ${path} already exist. Type 'y' without quotes to override it. `, (response) => {
+  rl.question(`The file at ${path} already exist. Type "y" without quotes to override it. `, (response) => {
     if (response.toLowerCase() === 'y') {
       writeFileToSystem(path, body);
     }
